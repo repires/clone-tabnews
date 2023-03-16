a9450855d538c458084911d06e3728dd1d92ae6a
Renato Pires <repires@gmail.com>
2023-03-16 15:53:33 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import { useState } from "react";
-
 function Home(){
-    return (
+    return <div>
         <h1>Hi <img src="https://user-images.githubusercontent.com/18350557/176309783-0785949b-9127-417c-8b55-ab5a4333674e.gif" />My name is Renato (Ray) Pires and I'm back in the game! 😃</h1>
-    )
-
+    </div>
 }
 export default Home;
