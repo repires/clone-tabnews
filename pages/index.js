94ac98c13857f8e5ce9b08153f22274c0d2221b5
Renato Pires <contact@raydev.ca>
2023-04-14 09:33:54 -0600

    dark + light mode

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const themeStyles = {
+    backgroundColor: darkMode ? '#333' : '#fff',
+    color: darkMode ? '#ddd' : '#333',
+  };
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <main>
-        <div style={{ maxWidth: '800px', padding: '2rem', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-          <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>
+    <div className={`d-flex justify-content-center align-items-center vh-100 ${darkMode ? 'bg-dark' : 'bg-light'}`}>
+      <main className="container">
+        <div className="card mx-auto" style={{ maxWidth: '800px', padding: '2rem', ...themeStyles }}>
+          <h1 className="h2 mb-4" style={{ fontSize: '2rem', marginBottom: '1rem' }}>
             Hi{' '}
             <img
               src="https://user-images.githubusercontent.com/18350557/176309783-0785949b-9127-417c-8b55-ab5a4333674e.gif"
               alt="smiling face with sunglasses"
+              className="img-fluid"
               style={{ height: '2rem', verticalAlign: 'middle' }}
             />
             My name is Renato (Ray) Pires and I'm back in the game! 😃
           </h1>
-          <p style={{ margin: '1rem 0' }}>
+          <p>
             As a computer engineer with many years of experience but no knowledge of new technologies with web development, I have recently sold my company and am now considering a career change. My wife has been an incredible source of motivation and support during this transition.
           </p>
-          <p style={{ margin: '1rem 0' }}>
+          <p>
             Despite my hesitations and uncertainty about entering a new field, my wife has been encouraging me to explore the world of web development. She recognizes my passion for problem-solving and my eagerness to learn new things, and she believes that web development could be the perfect fit for me.
           </p>
-          <p style={{ margin: '1rem 0' }}>
+          <p>
             My wife has been providing me with resources, such as online courses and tutorials, to help me gain the necessary skills to succeed in this field. She has also been connecting me with her network of professionals in the industry to offer guidance and advice.
           </p>
-          <p style={{ margin: '1rem 0' }}>
+          <p>
             Most importantly, my wife's unwavering belief in me has given me the confidence to take the leap and pursue this new career path. Her encouragement and support have been invaluable, and I am grateful to have her by my side as I navigate this exciting new chapter in my life.
           </p>
+          <button className={`btn mt-3 ${darkMode ? 'btn-light' : 'btn-dark'}`} onClick={toggleDarkMode}>
+            Toggle {darkMode ? 'Light' : 'Dark'} Mode
+          </button>
         </div>
       </main>
     </div>
